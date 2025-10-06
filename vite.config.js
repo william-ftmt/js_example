957715466e5efcea6c195185820b634fc4cefe40
William Wang <wwang@noggin.io>
2025-10-06 13:32:17 +1100

    change base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/js_example/',
   build: {
     outDir: 'docs',  // build output folder
     emptyOutDir: true // clean folder before build
